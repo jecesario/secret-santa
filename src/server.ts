26bd7b757958f426a236bef1339ec903167ce83b
Jean Cesario <lawjp1@gmail.com>
2024-04-19 15:27:26 -0300

    creating first route and request interceptor

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,18 @@ import express from 'express';
 import cors from 'cors';
 import https from 'https';
 import http from 'http';
+import siteRoutes from './routes/site';
+import {requestInterceptor} from "./utils/requestInterceptor";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.all('*', requestInterceptor);
+
+app.use('/', siteRoutes);
 
 const runServer = (port: number, server: http.Server) => {
     server.listen(port, () => {
